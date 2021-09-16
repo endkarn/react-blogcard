ef552b1e9f26f62a4ddae1512201be3ceed94dc1
END_K <end_karn@hotmail.com>
2021-09-16 15:14:22 +0700

    React component and state handling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 import './App.css';
 
 import BlogCard from './BlogCard';
 import { isArrayEmpty } from './Utills'
 
-function App() {
-  // const blogArray = null
-  const blogArray = [
+class App extends React.Component {
+  state = {
+    showBlog: true
+  }
+
+  blogArray = [
     {
       id: 1,
       title: 'first book edition',
@@ -24,20 +29,48 @@ function App() {
     }
   ]
 
-  if (isArrayEmpty(blogArray)) {
-    return ''
-  }
-  const blogCard = blogArray.map((item, pos) => {
-    return (
-      <BlogCard key={pos} title={item.title} description={item.description} id={item.id}/>
-    )
-  })
 
-  return (
-    <div className="App">
-      {blogCard}
-    </div>
-  )
+  blogCard = ''
+
+  // blogCard = isArrayEmpty(this.blogArray) ? [] : this.blogArray.map((item, pos) => {
+  //   return (
+  //     <BlogCard key={pos} title={item.title} description={item.description} id={item.id}/>
+  //   )
+  // })
+
+
+  onHideBtnClick = () => {
+    // let updatedState = !this.state.showBlog;
+    // this.setState({
+    //   showBlog: updatedState
+    // })
+
+    this.setState((prevState, prevProps) => {
+      return {showBlog: !prevState.showBlog}
+    })
+  }
+
+  render() {
+    const isEmptyArray = isArrayEmpty(this.blogArray)
+
+    if (!isEmptyArray){
+      this.blogCard = this.blogArray.map((item, pos) => {
+        return (
+          <BlogCard key={pos} title={item.title} description={item.description} id={item.id}/>
+        )
+      })
+    }
+
+    return(
+      <div className="App">
+      <button onClick={this.onHideBtnClick}>{this.state.showBlog ? 'Hide List' : 'Show List'}</button>
+      <br></br>
+      {
+        this.state.showBlog ? this.blogCard : null
+      }
+    </div> 
+    )
+  }
 }
 
 export default App;
--- src/BlogCard.js
+++ src/BlogCard.js
@@ -2,16 +2,19 @@ import React from "react";
 
 import { dumpLogs } from "./Utills";
 
+// import './BlogCard.css'
+import classes from './BlogCard.module.css'
+
 const BlogCard = (properties) => {
 
     dumpLogs(properties)
 
     return (
-        <div className="BlogCard" id={properties.id}>
+        <div className={classes.NewBlogCard} id={properties.id}>
             <h3>{properties.title}</h3>
             <p>{properties.description}</p>
         </div>
     )
 }
 
-export default BlogCard
+export default BlogCard 
